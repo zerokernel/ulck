9f4bee2137676955b1280090c4dd50e51f90741a
wangchenxunum <wangchenxunum@qq.com>
2018-02-11 20:21:47 +0800

    v0.1.0: 重构

--- test/index.js
+++ test/index.js
@@ -3,20 +3,21 @@
 const {MongoClient} = require('mongodb');
 const ulck = require("../");
 async function run () {
-	let db = await MongoClient.connect('mongodb://localhost:27017/ulck');
-	const {create, set, remove, get, query} = await ulck(db.collection("list"));
+	const {create, set, remove, get, query} = await ulck(
+		(await MongoClient.connect('mongodb://localhost:27017/ulck')).db("ulck").collection("list")
+	);
 	let c1 = [], c2 = [], c3 = [];
 	for(let i = 0; i < 20; i++) {
 		c1[i] = await create({title:"测试" + i});
-		await console.log(`c1[${i}]:`, c1[i]);
+		console.log(`c1[${i}]:`, c1[i]);
 	}
 	for(let i = 0; i < 20; i++) {
 		c2[i] = await create({title:"测试" + i,pid:c1[i]});
-		await console.log(`c2[${i}]:`, c2[i]);
+		console.log(`c2[${i}]:`, c2[i]);
 	}
 	for(let i = 0; i < 20; i++) {
 		c3[i] = await create({title:"测试" + i,pid:c2[2]});
-		await console.log(`c3[${i}]:`, c3[i]);
+		console.log(`c3[${i}]:`, c3[i]);
 	}
 }
 run().catch(console.log).then(x=>process.exit(0))
